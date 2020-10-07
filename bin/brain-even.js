11d294a8ba23309ea060b119aeef68935210b0f3
Andrei Prokofev <palych322@gmail.com>
2020-10-07 16:10:57 +0300

    code refactoring

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,34 +1,5 @@
 #! /usr/bin/env node
 
-import readlineSync from 'readline-sync';
-
-const isEven = (num) => num % 2 === 0;
-
-const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
-
-const getCorrectAnswer = () => {
-  const question = getRandomInt(100);
-  const correctAnswer = isEven(question) ? 'yes' : 'no';
-  return [question, correctAnswer];
-};
-
-const evenGame = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  for (let roundCount = 1; roundCount <= 3; roundCount += 1) {
-    const [question, correctAnswer] = getCorrectAnswer();
-    console.log(`Question: ${question}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === correctAnswer) console.log('Correct!');
-    else {
-      console.log(`'${answer}' is a wrong answer ;(. Correct answer was '${correctAnswer}'`);
-      console.log(`Let's try again ${name}`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`);
-};
+import evenGame from '../src/games/brainEven.js';
 
 evenGame();
