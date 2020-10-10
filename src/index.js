13164d8f17b9719abc9e41839cd93a9cac812746
Andrei Prokofev <palych322@gmail.com>
2020-10-10 18:01:35 +0300

    code refactoring

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import readlineSync from 'readline-sync';
+import promptly from 'promptly';
 
-const runGame = (gameRules, getAnswerAndQuestion) => {
+const askName = async () => {
+  const name = await promptly.prompt('May I have your name? ');
+  return name;
+};
+
+const askAnswer = async () => {
+  const answer = await promptly.prompt('Your answer: ');
+  return answer;
+};
+
+const runGame = async (gameRules, getAnswerAndQuestion) => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
+  const name = await askName();
   console.log(`Hello, ${name}!`);
   console.log(gameRules);
 
   for (let roundCount = 1; roundCount <= 3; roundCount += 1) {
     const [question, correctAnswer] = getAnswerAndQuestion();
     console.log(`Question: ${question}`);
-    const answer = readlineSync.question('Your answer: ');
+    const answer = await askAnswer(); // eslint no-await-in-loop disabled this line
     if (answer === correctAnswer) console.log('Correct!');
     else {
       console.log(`'${answer}' is a wrong answer ;(. Correct answer was '${correctAnswer}'`);
